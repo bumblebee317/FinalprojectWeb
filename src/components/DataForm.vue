d10877bb05fd27c5bf9561fb6991718cc8e888fe
bumblebee317 <Troywittke@ymail.com>
2025-12-11 21:18:27 -0500

    Add files via upload

---
+++ src/components/DataForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+<div v-if="item.published" class="TBox">
+<p>
+ Item ID: {{ item.id }}   
+</p>
+<p>
+Title: {{ item.title }}
+</p>
+<p>
+Status: {{ item.published }}
+</p>
+</div>
+<div v-else class="FBox">
+<p>
+Item ID: {{ item.id }}   
+</p>
+<p>
+Title: {{ item.title }}
+</p>
+<p>
+Status: {{ item.published }}
+</p>
+</div>
+</div>
+</template>
+
+<script>
+    export default{
+        props:["item"]
+    }
+</script>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+ <h2>Login</h2>
+<input v-model="password" placeholder="Enter Password">
+<button @click="checkpass">
+Login
+</button>
+<p>Password: cardinals</p>
+</div>
+</template>
+
+<script>
+export default {
+    props: ["onLogin"],
+    data(){
+        return {
+            password: "cardinals"
+        }
+    },
+methods: {
+    checkpass() {
+        if(this.password === "cardinals"){
+            this.onLogin()
+        } else {
+            alert("Wrong Password")
+        }
+    }
+}
+
+
+
+}
+
+
+
+</script>
